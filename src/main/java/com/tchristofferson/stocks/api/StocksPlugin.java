b130601fbe4948c3b28f6f916a1e9e7c02cc1645
tchristofferson <longleadtrc@gmail.com>
2021-09-20 23:54:09 -0500

    Added functionality to fetch stock data returning a map

--- src/main/java/com/tchristofferson/stocks/api/StocksPlugin.java
+++ src/main/java/com/tchristofferson/stocks/api/StocksPlugin.java
@@ -1,6 +1,5 @@
 package com.tchristofferson.stocks.api;
 
-import com.tchristofferson.stocks.api.core.IStock;
 import com.tchristofferson.stocks.api.core.IStockData;
 import com.tchristofferson.stocks.api.core.IStockDataRequester;
 import com.tchristofferson.stocks.api.core.IStockStorage;
@@ -8,19 +7,30 @@ import org.bukkit.plugin.Plugin;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Future;
 
 public interface StocksPlugin extends Plugin {
 
     IStockStorage getStockStorage();
 
-    Future<IStockData> getStockData(String symbol);
-
     Future<IStockData> getStockData(String symbol, boolean requireLive);
 
-    Future<IStockData[]> getStockData(List<String> symbols);
+    default Future<IStockData> getStockData(String symbol) {
+        return getStockData(symbol, false);
+    }
 
     Future<IStockData[]> getStockData(List<String> symbols, boolean requireLive);
+
+    default Future<IStockData[]> getStockData(List<String> symbols) {
+        return getStockData(symbols, false);
+    }
+
+    Future<Map<String, IStockData>> getStockDataMap(List<String> symbols, boolean requireLive);
+
+    default Future<Map<String, IStockData>> getStockDataMap(List<String> symbols) {
+        return getStockDataMap(symbols, false);
+    }
 
     default Future<IStockData[]> getStockData(String... symbols) {
         return getStockData(Arrays.asList(symbols));
--- src/main/java/com/tchristofferson/stocks/api/core/IStockDataRequester.java
+++ src/main/java/com/tchristofferson/stocks/api/core/IStockDataRequester.java
@@ -1,5 +1,7 @@
 package com.tchristofferson.stocks.api.core;
 
+import java.util.Map;
+
 public interface IStockDataRequester {
 
     /**
@@ -18,6 +20,14 @@ public interface IStockDataRequester {
     IStockData[] requestStockData(boolean requireLive);
 
     /**
+     * <b>Method will perform a blocking web request!</b><br>
+     * Get stocks' data
+     * @param requireLive If {@code false}, the data returned will be from the cache if it exists, otherwise it will fetch from the internet
+     * @return A map of stock data where the key is the symbol
+     */
+    Map<String, IStockData> requestStockDataMap(boolean requireLive);
+
+    /**
      * <b>Method will perform blocking web request!</b><br>
      * Get stocks' split data
      * @return An array of stock data
@@ -26,9 +36,23 @@ public interface IStockDataRequester {
 
     /**
      * <b>Method will perform blocking web request!</b><br>
+     * Get stocks' split data
+     * @return A map of stock data where the key is the symbol
+     */
+    Map<String, IStockData> requestStockSplitsMap();
+
+    /**
+     * <b>Method will perform blocking web request!</b><br>
      * Get stocks' data with split data
      * @return An array of stock data
      */
     IStockData[] requestStockDataAndSplits();
+
+    /**
+     * <b>Method will perform blocking web request!</b><br>
+     * Get stocks' data with split data
+     * @return A map of stock data where the key is the symbol
+     */
+    Map<String, IStockData> requestStockDataAndSplitsMap();
 
 }
